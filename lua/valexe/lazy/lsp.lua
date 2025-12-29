c8b0487c9df847517fadf83561f22251ab6c0e09
valexe <valantis.alatsas@gmail.com>
2025-12-29 05:59:41 +0200

    lsp.lua now works with mason 2.0

--- lua/valexe/lazy/lsp.lua
+++ lua/valexe/lazy/lsp.lua
@@ -1,47 +1,36 @@
-local root_files = {
-  '.luarc.json',
-  '.luarc.jsonc',
-  '.luacheckrc',
-  '.stylua.toml',
-  'stylua.toml',
-  'selene.toml',
-  'selene.yml',
-  '.git',
-}
-
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
         "stevearc/conform.nvim",
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
+        "mason-org/mason.nvim",
+        "mason-org/mason-lspconfig.nvim",
     },
-
     config = function()
+        -- initialize conform
         require("conform").setup({
-            formatters_by_ft = {
+            formaters_by_ft = { -- add formaters here
+                lua = {
+                    "stylua",
+                }
             }
         })
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
 
+        -- initialize completion
+        local cmp = require("cmp")
+
+        -- initialize lsp notification
         require("fidget").setup({})
+
+        -- initialize mason
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = {
+            ensure_installed = { -- add language servers here
                 "lua_ls",
                 "rust_analyzer",
                 "gopls",
@@ -56,59 +45,36 @@ return {
                 "clangd",
                 "cssls",
             },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                workspace = {
-                                    userThirdParty = "/home/valexe/.lualib",
-                                    checkThirdParty = "Apply"
-                                },
-                                format = {
-                                    enable = true,
-                                    defaultConfig = {
-                                        indent_style = "space",
-                                        indent_size = "2",
-                                    }
-                                },
-                            }
-                        }
-                    }
-                end,
-            }
         })
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        -- set up language servers
+        -- defualt config
+        vim.lsp.config("*", {})
+        -- lua specific
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                            os.getenv("HOME") .. "/.lualib",
+                        },
+                    },
+                },
+            },
+        })
 
+        local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+        -- setup completion
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require("luasnip").lsp_expand(args.body)
                 end,
             },
             mapping = cmp.mapping.preset.insert({
@@ -117,25 +83,26 @@ return {
                 ['<C-y>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
+            sources = cmp.config.sources(
+                {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                },
+                {
+                    { name = 'buffer' },
+                })
             })
-        })
 
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end
-}
-
+            -- initialize diagnostics
+            vim.diagnostic.config({
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = true,
+                    header = "",
+                    prefix = "",
+                },
+            })
+        end,
+    }
